94fbb41b4c115dea212185334fb7ca99b69d2339
Pushpak741 <pushpakganagoni@gmail.com>
2020-06-12 11:25:23 +0530

    f commit

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
 from flask import Flask
 from flask_restful import Api
-from resources.emp import Emp
+from flask_jwt_extended import JWTManager
+from resources.emp import Emp,Emplogin
 
 app=Flask(__name__)
-
+app.config['JWT_SECTRET_KEY']='coscskillup'
 app.config['PROPAGATE_EXCEPTIONS']=True
 app.config['PREFERRED_URL_SCHEME']='https'
 api=Api(app)
+jwt=JWTManager(app)
 
 api.add_resource(Emp,'/emp')
+api.add_resource(Emplogin,'/login')
 
 if __name__ == "__main__":
     app.run()
